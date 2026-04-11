80130f41628afa345d3c1b1eab136df5dc4514c3
Kaan NASURLA <kaan.nasurla@stud.acs.upb.ro>
2026-04-11 11:44:25 +0000

    Edit maze.asm

--- src/task2/maze.asm
+++ src/task2/maze.asm
@@ -1,30 +1,14 @@
-; %include "../include/io.mac"
-; extern printf
-global filter_drivers
+extern printf
+extern position
+global solve_labyrinth
 
-; Driver struct layout (8 bytes total):
-;   Offset 0  (4 bytes) - access_level   : 32-bit integer, value 0-100
-;   Offset 4  (2 bytes) - nationality    : 0x4445='DE', 0x4F52='RO', 0x4B55='UK', 0x5246='FR'
-;   Offset 6  (1 byte)  - is_phantom     : 1=fake, 0=real
-;   Offset 7  (1 byte)  - flags          : bit0=under_investigation, bit1=priority_clearance
-
-section .bss
-    output_buffer resb 8192     ; static buffer: 1024 drivers * 8 bytes each
-
-section .data
-    ; optionally, add debug format strings here if needed
+; you can declare any helper variables in .data or .bss
 
 section .text
 
-; filter_drivers(driver *input_array, int num_drivers)
-;   rdi = address of input driver array
-;   rsi = number of drivers in input array
-;
-; returns:
-;   rax = address of output_buffer (filtered drivers)
-;   rbx = number of drivers written to output_buffer
+; void solve_labyrinth(unsigned int *out_line, unsigned int *out_col, unsigned int m, unsigned int n, char **maze);
 
-filter_drivers:
+solve_labyrinth:
     ;; DO NOT MODIFY
     push    rbp
     mov     rbp, rsp
@@ -33,19 +17,26 @@ filter_drivers:
     push    r13
     push    r14
     push    r15
+
+    mov     r12, rdi    ; unsigned int *out_line
+    mov     r13, rsi    ; unsigned int *out_col
+    mov     r14, rdx    ; unsigned int m
+    mov     r15, rcx    ; unsigned int n
+    mov     rbx, r8     ; char **maze
     ;; DO NOT MODIFY
-
-    ;; Your code starts here
-
-
-    ;; Your code ends here
+   
+    ;; YOUR CODE STARTS HERE
+    
+    
+    
+    ;; YOUR CODE ENDS HERE
 
     ;; DO NOT MODIFY
     pop     r15
     pop     r14
     pop     r13
     pop     r12
+    pop     rbx
     pop     rbp
     ret
-
     ;; DO NOT MODIFY
