a662b7a19dbf00c95a914357df9cb603b239d5b9
Ilinca-Ioana STRUȚU <ilinca_ioana.strutu@stud.acs.upb.ro>
2026-04-11 15:42:08 +0000

    Edit subtask3.asm

--- src/task3/subtask3.asm
+++ src/task3/subtask3.asm
@@ -1,24 +1,21 @@
-; write the structures
-; struc date
-; 		???
-; endstruct
-
-; struct ticket
-; 		???
+; write the structure
+; struct flight
+; 		TODO
 ; endstruct
 
 
 section .text
+
+;; DO NOT MODIFY
 global sort_and_return
 
-; int sort_and_return(struct ticket* tickets, int nrTickets, 
-;                      struct ticket* bestTicket, char destination[32])
-; rdi = tickets (pointer)
-; rsi = nrTickets (value)
-; rdx = bestTicket (pointer to pre-allocated struct)
+; int sort_and_return(struct flight* flights, int nrFlights, 
+;                      struct flight* bestFlight, char destination[32])
+; rdi = flights (pointer)
+; rsi = nrFlights (value)
+; rdx = bestFlight (pointer to pre-allocated struct)
 ; rcx = destination (pointer to 32-byte string)
 sort_and_return:
-	;; DO NOT MODIFY
 	push rbp
 	mov rbp, rsp
 	push rbx
