f3187dfe6e72978cb9573e2ee677d56c025209fd
Pasagali Alessio <essio.pg@gmail.com>
2026-05-02 16:54:21 +0300

    Final changes to subtask2 and almost done with subtask3 in task3

--- src/task3/subtask2.asm
+++ src/task3/subtask2.asm
@@ -1,7 +1,16 @@
 ; write the structures
-; struct flight
-; 		TODO
-; endstruct
+struc flight
+    .destination: resb 32
+    .depart_day: resb 1
+    .depart_hour: resb 1
+    .depart_minutes: resb 1
+    .arrival_day: resb 1
+    .arrival_hour: resb 1
+    .arrival_minute: resb 1
+    .bag_weight: resw 1
+    .delay_minutes: resb 1
+    .delay_hours: resb 1
+endstruc
 
 
 section .text
@@ -25,6 +34,38 @@ filter_flights:
 	push r15
 	;; DO NOT MODIFY
 	;; Your code starts here
+	mov r8d, dword [rdx];number of flights
+	mov r9d,0
+	mov r11,rdx
+filter_loop:
+	cmp r8d,0
+	je end_filter
+	movzx r10d,word[rdi+38]
+	cmp r10d,ecx
+	jl skip_flight
+	mov rax, [rdi]
+    mov [rsi], rax
+    mov rax, [rdi + 8]
+    mov [rsi + 8], rax
+    mov rax, [rdi + 16]
+    mov [rsi + 16], rax
+    mov rax, [rdi + 24]
+    mov [rsi + 24], rax
+	;we copy the rest of bytes
+    mov rax, [rdi + 32]
+    mov [rsi + 32], rax
+	;we move the last 2 bytes
+    mov ax, [rdi + 40]
+    mov [rsi + 40], ax
+	inc r9d
+	add rsi,42
+skip_flight:
+	add rdi,42
+	dec r8d
+	jnz filter_loop
+end_filter:
+	mov dword[r11],r9d
+	
 
 	;; Your code ends here
 	;; DO NOT MODIFY
--- src/task3/subtask3.asm
+++ src/task3/subtask3.asm
@@ -1,7 +1,16 @@
 ; write the structure
-; struct flight
-; 		TODO
-; endstruct
+struc flight
+    .destination: resb 32
+    .depart_day: resb 1
+    .depart_hour: resb 1
+    .depart_minutes: resb 1
+    .arrival_day: resb 1
+    .arrival_hour: resb 1
+    .arrival_minute: resb 1
+    .bag_weight: resw 1
+    .delay_minutes: resb 1
+    .delay_hours: resb 1
+endstruc
 
 
 section .text
@@ -25,7 +34,71 @@ sort_and_return:
 	push r15
 	;; DO NOT MODIFY
 	;; Your code starts here
+	mov r13,rdx
+	mov r14,rcx
+	mov r15,rdi
+	cmp rsi,1
+	jle search_destination
 
+	mov r8d,esi;exterior limit
+ext_loop:
+	mov r12d,0;swaps_made=false
+	mov rcx,r15;start address of the array
+	mov r9d,esi;contor
+	dec r9d
+inner_loop:
+	movzx eax,byte[rbx+35];A flight day
+	movzx edx,byte[rbx+35+42];B flight day
+	cmp eax,edx
+	jg swap_flights
+	jl no_swap
+	movzx eax,byte[rbx+36];A flight hour
+	movzx edx,byte[rbx+36+42];B flight hour
+	cmp rax,edx
+	jg swap_flights
+	jl no_swap
+	movzx eax,byte[rbx+37];A flight minute
+	movzx edx,byte[rbx+37+42];B flight minute
+	cmp eax,edx
+	jg swap_flights
+	jl no_swap
+	movzx eax,word[rbx+38];A flight luggage weight
+	movzx edx,word[rbx+38+42];B flight luggage weight
+	cmp eax,edx
+	jl swap_flights
+	jmp no_swap
+swap_flights:
+	mov r12d,1;swap made, flag set to 1
+	push qword[rbx]
+	mov rax,[rbx+42]
+	mov [rbx],rax
+	pop qword[rbx+42]
+
+	push qword [rbx + 8]
+    mov rax, [rbx + 42 + 8]
+    mov [rbx + 8], rax
+    pop qword [rbx + 42 + 8]
+
+	push qword [rbx + 16]
+    mov rax, [rbx + 42 + 16]
+    mov [rbx + 16], rax
+    pop qword [rbx + 42 + 16]
+
+	push qword [rbx + 24]
+    mov rax, [rbx + 42 + 24]
+    mov [rbx + 24], rax
+    pop qword [rbx + 42 + 24]
+
+	push qword [rbx + 32]
+    mov rax, [rbx + 42 + 32]
+    mov [rbx + 32], rax
+    pop qword [rbx + 42 + 32]
+
+	push word [rbx + 40]
+    mov ax, [rbx + 42 + 40]
+    mov [rbx + 40], ax
+    pop word [rbx + 42 + 40]
+	
 	;; Your code ends here
 	;; DO NOT MODIFY
 	pop r15
