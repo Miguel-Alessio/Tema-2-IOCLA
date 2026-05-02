9581317aa9788e76884174cfe2edf36459be98f0
Pasagali Alessio <essio.pg@gmail.com>
2026-05-02 14:30:54 +0300

    Adding to subtask1.asm in task3

--- src/task3/subtask1.asm
+++ src/task3/subtask1.asm
@@ -1,16 +1,16 @@
 ; write the structures. make sure it fits the layour in the README
-struct flight
-	.destination: resw 32
+struc flight
+	.destination: resb 32
 	.depart_day: resb 1
 	.depart_hour: resb 1
 	.depart_minutes: resb 1
 	.arrival_day: resb 1
 	.arrival_hour: resb 1
 	.arrival_minute: resb 1
-	.bag_weight: resw 2
+	.bag_weight: resw 1
 	.delay_minutes: resb 1
 	.delay_hours: resb 1
-enstruct
+endtruc
 
 section .text
 ;; DO NOT MODIFY
@@ -29,7 +29,11 @@ apply_delay:
 	push r15
 	;; DO NOT MODIFY
 	;; Your code starts here
-
+flight_loop:
+	;we add to r8d the delay minutes
+	movzx r8d, byte[rdi+40]
+	;we add to r9d the delay delay hours
+	movzx r9d,byte[rdi+41]
 	;; Your code ends here
 	;; DO NOT MODIFY
 	pop r15
