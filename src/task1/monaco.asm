22e1d0b29c9ad30a5187b42941f03c560763346a
Pasagali Alessio <essio.pg@gmail.com>
2026-05-02 13:45:53 +0300

    Initial commit

--- src/task1/monaco.asm
+++ src/task1/monaco.asm
@@ -3,7 +3,6 @@ section .text
 ;; DO NOT MODIFY
 global fix_lap_times
 
-
 fix_lap_times:
     push rbp
     mov rbp, rsp
@@ -15,7 +14,63 @@ fix_lap_times:
     ;; DO NOT MODIFY
     ;; YOUR CODE STARTS HERE
 
+    ; initializam indexul loop-ului cu 0
+    mov r12, 0
+    ; initializam contorul de erori cu 0
+    mov r13, 0
 
+start_loop:
+    cmp r12, rdx
+    je stop
+
+    mov al, byte [rsi+r12]
+    ; 1 reprezinta un tur corupt
+    cmp al, 1
+    je fixer
+
+    ; 4 este dimensiunea in bytes a unui int
+    mov ebx, dword [rdi+r12*4]
+    ; 4 este dimensiunea in bytes a unui int
+    mov dword [rcx+r12*4], ebx
+
+    inc r12
+    jmp start_loop
+
+fixer: 
+    inc r13
+    ; 0 reprezinta indexul primului sofer
+    cmp r12, 0
+    je first_driver
+    mov r9, rdx
+    ; scadem 1 pentru a gasi indexul ultimului sofer
+    sub r9, 1
+    cmp r12, r9
+    je last_driver
+    ; -4 reprezinta offsetul pentru soferul anterior
+    mov eax, dword [rdi+r12*4-4]
+    ; +4 reprezinta offsetul pentru soferul urmator
+    add eax, dword [rdi+r12*4+4]
+    ; shiftarea la dreapta cu 1 bit impartire la 2
+    shr eax, 1 
+    jmp save_fix
+
+first_driver:
+    ; +4 reprezinta offsetul pentru soferul urmator
+    mov eax, dword [rdi+r12*4+4]
+    jmp save_fix
+
+last_driver:
+    ; -4 reprezinta offsetul pentru soferul anterior
+    mov eax, dword [rdi+r12*4-4]
+
+save_fix:
+    ; 4 este dimensiunea in bytes a unui int
+    mov dword [rcx+r12*4], eax
+    inc r12
+    jmp start_loop   
+
+stop:
+    mov dword [r8], r13d
 
     ;; YOUR CODE ENDS HERE
     ;; DO NOT MODIFY
@@ -23,7 +78,7 @@ fix_lap_times:
     pop r14
     pop r13
     pop r12
-    pop rbx
+    pop rbx 
     pop rbp
     ret
     ;; DO NOT MODIFY
--- src/task2/paris.asm
+++ src/task2/paris.asm
@@ -1,5 +1,3 @@
-; you can declare any helper variables in .data or .bss
-
 section .text
 
 ;; DO NOT MODIFY
@@ -21,8 +19,125 @@ solve_labyrinth:
     mov     rbx, r8
     ;; DO NOT MODIFY
     ;; YOUR CODE STARTS HERE
+    
+    ;r10 si r11 sunt coordonatele actuale
+    ;rdi si rsi sunt coordonatele la pasul anterior
 
+    ;curatam gunoiul din partea superioara a dimensiunilor pe 64 de biti
+    mov r14d, r14d
+    mov r15d, r15d
+    ;obtinem m-1 si n-1
+    sub r14, 1
+    ;
+    sub r15, 1
+    ;linia r10 si coloana r11
+    mov r10, 0
+    ;
+    mov r11, 0
+    ;memoria celulei anterioare (rdi = linia, rsi = coloana)
+    ;initializam cu -1 ca sa nu se potriveasca cu nicio coordonata la primul pas
+    mov rdi, -1
+    ;
+    mov rsi, -1
+start_maze:
+    ;conditia de victorie
+    cmp r10, r14
+    je win
+    cmp r11, r15
+    je win
+    
+check_right:
+    cmp r11, r15
+    je check_down
+    mov r8, r11
+    inc r8
+    ;verificam daca e celula din care tocmai am venit
+    cmp r10, rdi
+    jne do_check_r
+    cmp r8, rsi
+    je check_down
+do_check_r:
+    ;
+    mov r9, qword [rbx+r10*8]
+    mov al, byte [r9+r8]
+    ;
+    cmp al, '0'
+    jne check_down
+    ;salvam pozitia curenta la istoric si facem pasul
+    mov rdi, r10
+    mov rsi, r11
+    mov r11, r8
+    jmp start_maze
+check_down:
+    cmp r10, r14
+    je check_left
+    mov r8, r10
+    inc r8
+    cmp r8, rdi
+    jne do_check_d
+    cmp r11, rsi
+    je check_left
+do_check_d:
+    ;
+    mov r9, qword [rbx+r8*8]
+    mov al, byte [r9+r11]
+    ;
+    cmp al, '0'
+    jne check_left
+    mov rdi, r10
+    mov rsi, r11
+    mov r10, r8
+    jmp start_maze
+check_left:
+    ;
+    cmp r11, 0
+    je check_up
+    mov r8, r11
+    dec r8
+    cmp r10, rdi
+    jne do_check_l
+    cmp r8, rsi
+    je check_up
+do_check_l:
+    ;
+    mov r9, qword [rbx+r10*8]
+    mov al, byte [r9+r8]
+    ;
+    cmp al, '0'
+    jne check_up
+    mov rdi, r10
+    mov rsi, r11
+    mov r11, r8
+    jmp start_maze
 
+check_up:
+    ;
+    cmp r10, 0
+    je stuck
+    mov r8, r10
+    dec r8
+    cmp r8, rdi
+    jne do_check_u
+    cmp r11, rsi
+    je stuck
+
+do_check_u:
+    ;
+    mov r9, qword [rbx+r8*8]
+    mov al, byte [r9+r11]
+    ;
+    cmp al, '0'
+    jne stuck
+    mov rdi, r10
+    mov rsi, r11
+    mov r10, r8
+    jmp start_maze
+stuck:
+    jmp win
+win:
+    ; Salvam valorile
+    mov dword [r12], r10d
+    mov dword [r13], r11d
 
     ;; YOUR CODE ENDS HERE
     ;; DO NOT MODIFY
